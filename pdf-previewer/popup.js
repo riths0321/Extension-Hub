dae9a624369a05d166699792ef4e90c89766d030
Ritik Bharatpure <ritiksahu0321@gmail.com>
2026-02-24 12:18:41 +0530

    Enhanced UI and updated overall functionality in  pdf-previewer

--- pdf-previewer/popup.js
+++ pdf-previewer/popup.js
@@ -1,7 +1,7 @@
-
 const fileInput = document.getElementById("fileInput");
 const pdfViewer = document.getElementById("pdfViewer");
 const selectFileBtn = document.getElementById("selectFileBtn");
+const openFullBtn = document.getElementById("openFullBtn");
 
 selectFileBtn.addEventListener("click", () => {
   fileInput.click();
@@ -13,4 +13,19 @@ fileInput.addEventListener("change", () => {
 
   const fileURL = URL.createObjectURL(file);
   pdfViewer.src = fileURL;
+
+  openFullBtn.disabled = false;
+
+  // Convert to base64 and store safely
+  const reader = new FileReader();
+  reader.onload = function () {
+    chrome.storage.local.set({ fullPDF: reader.result });
+  };
+  reader.readAsDataURL(file);
+});
+
+openFullBtn.addEventListener("click", () => {
+  chrome.tabs.create({
+    url: chrome.runtime.getURL("viewer.html")
+  });
 });
