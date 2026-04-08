9d9bfb73c207dca533deb1b5e9064131d0d6b4ef
Ritik Bharatpure <ritiksahu0321@gmail.com>
2026-04-08 11:51:20 +0530

    fixed light mode issue in website-performance

--- website-performance/popup.js
+++ website-performance/popup.js
@@ -57,9 +57,9 @@ function bindUiEvents() {
 
 function getStoredTheme() {
   try {
-    return localStorage.getItem(THEME_STORAGE_KEY) === "light" ? "light" : "dark";
+    return localStorage.getItem(THEME_STORAGE_KEY) === "dark" ? "dark" : "light";
   } catch {
-    return "dark";
+    return "light";
   }
 }
 
